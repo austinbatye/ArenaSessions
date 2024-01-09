24ee4960b33153a7eb9b52589a76ba6011dbe0c8
Austin Batye <aubatye@microsoft.com>
2024-01-09 14:35:54 -0500

    Documentation added

--- arena-sessions/src/app/page.tsx
+++ arena-sessions/src/app/page.tsx
@@ -1,5 +1,8 @@
 import HomePage from '../pages/page-layout/Home';
 
+/**
+ * Home entry component // top-level component in stack
+ */
 export default function Home() {
   return <HomePage />;
 }
--- arena-sessions/src/pages/page-layout/PageContent.tsx
+++ arena-sessions/src/pages/page-layout/PageContent.tsx
@@ -3,6 +3,9 @@ import { FunctionComponent } from 'react';
 
 import { useCharacters } from '@/hooks/useCharacters';
 
+/**
+ * Component responsible for rendering the main page content
+ */
 const PageContent: FunctionComponent = () => {
   const { isLoading, characters } = useCharacters();
 
